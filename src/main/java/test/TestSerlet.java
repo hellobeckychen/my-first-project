003a0cdb04d352e791c0a3b04e38c056a24c33b3
Becky <hellobeckychen@gmail.com>
2023-04-20 11:52:44 +0800

    update

--- src/main/java/test/TestSerlet.java
+++ src/main/java/test/TestSerlet.java
@@ -16,7 +16,7 @@ public class TestSerlet extends HttpServlet {
        
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("just test!");
+		response.getWriter().append("just test!!!");
 	}
 
 
